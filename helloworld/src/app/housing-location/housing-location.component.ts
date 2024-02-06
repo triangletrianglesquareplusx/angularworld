0d8f3907569bd82cdb46e3a18e46e290c85e083c
triangletrianglesquareplusx <preslavgetov@gmail.com>
2024-02-06 14:33:44 +0200

    minor issues with router differentiation

--- helloworld/src/app/housing-location/housing-location.component.ts
+++ helloworld/src/app/housing-location/housing-location.component.ts
@@ -1,10 +1,11 @@
 import { Component, Input } from '@angular/core';
 import { HousingLocation } from '../housinglocation';
+import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-housing-location',
   standalone: true,
-  imports: [],
+  imports: [RouterLink, RouterOutlet],
   template: `
     <section class="listing">
       <img
@@ -16,6 +17,7 @@ import { HousingLocation } from '../housinglocation';
       <p class="listing-location">
         {{ housingLocation.city }}, {{ housingLocation.state }}
       </p>
+      <a [routerLink]="['/details', housingLocation.id]">Learn More</a>
     </section>
   `,
   styleUrl: './housing-location.component.css',
