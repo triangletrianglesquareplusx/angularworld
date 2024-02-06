9cc5bbb6feaf8670ec48cb92613001884cf5a2d2
triangletrianglesquareplusx <preslavgetov@gmail.com>
2024-02-06 09:38:01 +0200

    adding interface to the housing component

--- helloworld/src/app/home/home.component.ts
+++ helloworld/src/app/home/home.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HousingLocationComponent } from '../housing-location/housing-location.component';
+import { HousingLocation } from '../housinglocation';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, HousingLocationComponent],
   template: `
     <section>
       <form>
@@ -11,7 +14,23 @@ import { Component } from '@angular/core';
         <button class="primary" type="button">Search</button>
       </form>
     </section>
+    <section class="results">
+      <app-housing-location></app-housing-location>
+    </section>
   `,
   styleUrl: './home.component.css',
 })
-export class HomeComponent {}
+export class HomeComponent {
+  readonly baseUrl = 'https://angular.io/assets/images/tutorials/faa';
+
+  housingLocation: HousingLocation = {
+    id: 9999,
+    name: 'Test Home',
+    city: 'Test city',
+    state: 'ST',
+    photo: `${this.baseUrl}/example-house.jpg`,
+    availableUnits: 99,
+    wifi: true,
+    laundry: false,
+  };
+}
